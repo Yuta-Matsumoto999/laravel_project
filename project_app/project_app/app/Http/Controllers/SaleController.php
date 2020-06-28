397942f8a9290f0f419764b353e552f102ec49d2
松本雄大 <y.matsumoto0723@gmail.com>
2020-06-28 13:19:41 +0900

    コントローラー編集

--- project_app/project_app/app/Http/Controllers/SaleController.php
+++ project_app/project_app/app/Http/Controllers/SaleController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
+use App\Cart;
+use App\Buy;
+use App\TagCategory;
+use App\User;
+use Auth;
 
 class SaleController extends Controller
 {
-    //
+    private $product;
+    private $cart;
+    private $buy;
+    private $tagCategory;
+    private $user;
+
+    public function __construct(Product $product, Cart $cart, Buy $buy, TagCategory $tagCategory, User $user)
+    {
+        $this->middleware('auth');
+        $this->product = $product;
+        $this->cart = $cart;
+        $this->buy = $buy;
+        $this->tagCategory = $tagCategory;
+        $this->user = $user;
+    }
+
+
 }
